ef53c633418235dffd5c9c77d096cd7cbe93c75f
sridhar reddy mallela <mallelasridhar6025@gmail.com>
2023-06-28 18:45:20 +0530

    third commit

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0sp44l+%p*$0+2a+d*qu8&au5g!4%)^813o-m6i&xzi=(u%6n6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
